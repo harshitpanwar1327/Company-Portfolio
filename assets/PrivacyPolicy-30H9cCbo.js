4320fe14bd7f0ee5a85217b6043b1af238c0b895
harshitpanwar1327 <harshitpanwar1327@gmail.com>
2026-03-23 01:29:01 +0530

    Updates

--- assets/PrivacyPolicy-30H9cCbo.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DwoNfFWN.js";const o=()=>r.jsx("div",{children:"PrivacyPolicy"});export{o as default};
--- assets/TermsAndConditions-Cleub5I_.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-DwoNfFWN.js";const o=()=>s.jsx("div",{children:"TemsAnsConditions"});export{o as default};
