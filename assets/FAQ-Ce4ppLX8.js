a84ac3d6c28c79f532af2a8c2f481bc7849f5a2d
harshitpanwar1327 <harshitpanwar1327@gmail.com>
2026-03-24 21:59:04 +0530

    Updates

--- assets/FAQ-Ce4ppLX8.js
+++ assets/FAQ-Ce4ppLX8.js
@@ -1,4 +1,4 @@
-import{c as a,r,j as e,b as l}from"./index-B3Ob2_k9.js";import{A as c}from"./index-Bijvw4-6.js";/**
+import{c as a,r,j as e,b as l}from"./index-vGXT9UL6.js";import{A as c}from"./index-Bm-qjCK3.js";/**
  * @license lucide-react v0.541.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/ScrollToTop-DF3B-5Iq.js
+++
@@ -1,1 +0,0 @@
-import{a as t,r}from"./index-B3Ob2_k9.js";const e=()=>{const{pathname:o}=t();return r.useEffect(()=>{window.scrollTo({top:0,left:0,behavior:"smooth"})},[o]),null};export{e as default};
